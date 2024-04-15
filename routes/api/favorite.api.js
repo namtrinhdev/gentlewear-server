bbe729b294888ef1710bde1d9184aeb7de1148b8
namtrinh <74081481+namtrinhdev@users.noreply.github.com>
2024-04-15 23:39:58 +0700

    Revert "Develop"

--- routes/api/favorite.api.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var favoriteCtrl = require('../../controllers/api/favorite.api.controller');
-
-router.get('/', favoriteCtrl.getFavorites);
-router.post('/toggle', favoriteCtrl.toggleFavorite);
-module.exports = router;         
