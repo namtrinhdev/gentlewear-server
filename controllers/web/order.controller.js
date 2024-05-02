7873fcd1e9a527f65a606621369f0f2d87448350
khanhtaph28259 <111049205+khanhtaph28259@users.noreply.github.com>
2024-05-02 09:22:01 +0700

    update

--- controllers/web/order.controller.js
+++ controllers/web/order.controller.js
@@ -4,8 +4,17 @@ const UserModel = require('../../models/user.model');
 exports.getOrderManagementPage = async (req, res) => {
     const page = parseInt(req.query.page) || 1; // lấy số trang hiện tại
     const itemsPerPage = 10; // đặt số lượng mục trên mỗi trang
+    const trangThai = req.query.trangThai; // lấy trạng thái từ query parameter
 
-    const transactions = await ThanhToanModel.find({})
+    // tạo một object query rỗng
+    let query = {};
+
+    // nếu trangThai tồn tại, thêm nó vào object query
+    if (trangThai) {
+        query.trangThai = trangThai;
+    }
+
+    const transactions = await ThanhToanModel.find(query)
         .skip((page - 1) * itemsPerPage) // bỏ qua số lượng mục cần bỏ qua
         .limit(itemsPerPage) // giới hạn số lượng mục thành itemsPerPage
         .populate({
